9fb18db720bb39a3e61af97c142c3149c4571a5f
jessicaidro <jessicaidro@gmail.com>
2022-08-25 23:55:41 -0300

    Automatizando a compilação de arquivos

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,4 @@
 import { Negociacao } from './models/negociacao.js';
-const negociacao = new Negociacao(new Date(), 10, 1005);
+
+const negociacao = new Negociacao(new Date(),10,1005);
 console.log(negociacao.volume);
